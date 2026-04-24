449b1d91eae28a67c5e8d8344b6d492efbc99263
oliversson1 <simkovic3@stud.uniza.sk>
2026-04-24 12:04:13 +0200

    prvy push

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import OSPABA.*;
 
 public class Main {
     public static void main(String[] args) {
-        Agent agent = new Agent(0, null,null);
-        System.out.println("Hello world!");
+        System.out.println(" wdeadds!");
     }
 }
